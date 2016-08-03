27cab858940b6bb2b63b67f509659c05a0b15357
Kaiser <kaiser@fapl.co>
2016-08-03 16:36:43 -0700

    Fixed navbar and added padding

--- js/navbar.js
+++ js/navbar.js
@@ -1,20 +1,24 @@
 $('#nav').affix({
   offset: {
-    top: $('header').height()-$('#nav').height() - 250
+    top: $('header').height()-$('#nav').height() - 150
   }
 });
 
 /* highlight the top nav as scrolling occurs */
-//$('body').scrollspy({ target: '#nav' })
+$('body').scrollspy({ target: '#nav' })
 
 /* smooth scrolling for scroll to top */
-/*$('.scroll-top').click(function(){
-$('body,html').animate({scrollTop:0},1000);
-});*/
+$('.scroll-top').click(function() {
+  $('body, html').animate({scrollTop: 0}, 1000);
+});
+
+$('.scroll-down').click(function() {
+  $('body, html').animate({scrollTop: 649}, 1000);
+});
 
 /* smooth scrolling for nav sections */
-/*$('#nav .navbar-nav li>a').click(function(){
+$('#nav .navbar-nav li>a').click(function() {
   var link = $(this).attr('href');
-  var posi = $(link).offset().top+20;
-  $('body,html').animate({scrollTop:posi},700);
-});*/
+  var pos = $(link).offset().top + 90;
+  $('body, html').animate({scrollTop: pos}, 800);
+});
