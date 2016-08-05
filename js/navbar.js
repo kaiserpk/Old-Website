fc9abd950a9b5a714b25e0b0a65d1822860c7517
Kaiser <kaiser@fapl.co>
2016-08-04 17:59:03 -0700

    Fixed margins and navbar

--- js/navbar.js
+++ js/navbar.js
@@ -7,13 +7,23 @@ $('#nav').affix({
 /* highlight the top nav as scrolling occurs */
 $('body').scrollspy({ target: '#nav' })
 
+/* brand fade in if navbar is near */
+$(window).scroll(function() {
+  if ( $(this).scrollTop() > 600 ) {
+    $('.scroll-top').fadeIn();
+  }
+  else {
+    $('.scroll-top').fadeOut();
+  }
+});
+
 /* smooth scrolling for scroll to top */
 $('.scroll-top').click(function() {
   $('body, html').animate({scrollTop: 0}, 1000);
 });
 
 $('.scroll-down').click(function() {
-  $('body, html').animate({scrollTop: 649}, 1000);
+  $('body, html').animate({scrollTop: 650}, 1500);
 });
 
 /* smooth scrolling for nav sections */
