83afad1f9eb223e22d3109e7d8d21ffb2301cfbf
Kaiser <kaiser@fapl.co>
2016-08-10 16:58:07 -0700

    Fixed responsive table and scroll

--- js/navbar.js
+++ js/navbar.js
@@ -5,30 +5,34 @@ $('#nav').affix({
 });
 
 /* highlight the top nav as scrolling occurs */
-$('body').scrollspy({ target: '#nav', offset: -39 })
+$('body').scrollspy({ target: '#nav', offset: -60 })
 
 /* brand fade in if navbar is near */
 $(window).scroll(function() {
   if ( $(this).scrollTop() > 550 ) {
-    $('.scroll-top').fadeIn();
+    $('.scroll-top').fadeIn(600);
   }
   else {
-    $('.scroll-top').fadeOut();
+    $('.scroll-top').fadeOut(600);
   }
 });
 
 /* smooth scrolling for scroll to top */
-$('.scroll-top').click(function() {
-  $('body, html').animate({scrollTop: 0}, 1000);
+$('.scroll-top').click(function(event) {
+  $('body, html').stop().animate({scrollTop: 0}, 1000, 'easeInOutExpo');
+  event.preventDefault();
 });
 
-$('.scroll-down').click(function() {
-  $('body, html').animate({scrollTop: 650}, 1500);
+$('.scroll-down').click(function(event) {
+  $('body, html').stop().animate({scrollTop: 650}, 1500, 'easeInOutExpo');
+  event.preventDefault();
 });
 
 /* smooth scrolling for nav sections */
-$('#nav .navbar-nav li>a').click(function() {
+$('#nav .navbar-nav li>a').click(function(event) {
   var link = $(this).attr('href');
-  var pos = $(link).offset().top + 90;
-  $('body, html').animate({scrollTop: pos}, 800);
+  var pos = $(link).offset().top + 60;
+  $('body, html').stop().animate({scrollTop: pos}, 1500, 'easeInOutExpo');
+  $('.navbar-collapse').collapse('hide');
+  event.preventDefault();
 });
